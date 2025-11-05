13f2945b50d8ce0514464aafd7ff7618376933fc
Peter Winther <dialga399@live.dk>
2025-11-05 14:17:06 +0100

    SpaceEvent is a file for event classes, to reduce file clutter for now. Can be split in the future if needed

---
+++ SpaceEvent.cs
@@ -0,0 +1,35 @@
+class DummySE : IEvent{
+	
+	// Method which does the events intended behavior
+	public void Trigger(){
+		Console.WriteLine("Dummy event");
+	}
+}
+
+class TextSE : IEvent{
+	
+	private string textToDisplay;
+	
+	public TextSE(string text){
+		textToDisplay = text;
+	}
+	
+	// Method which does the events intended behavior
+	public void Trigger(){
+		Console.WriteLine(textToDisplay);
+		Console.Write("\nPress enter key to continue... ");
+		Console.ReadLine();
+		Console.SetCursorPosition(0, Console.CursorTop - 1);
+		ClearCurrentConsoleLine();
+	}
+	
+	public static void ClearCurrentConsoleLine()
+	{
+		int currentLineCursor = Console.CursorTop;
+		Console.SetCursorPosition(0, Console.CursorTop);
+		Console.Write(new string(' ', Console.WindowWidth)); 
+		Console.SetCursorPosition(0, currentLineCursor);
+	}
+}
+
+
