4013c8445486e3eca901162828957f1d636cef4a
Peter <dialga399@live.dk>
2025-11-10 14:39:16 +0100

    Updated CommandExploreRoom to also list monsters in the current room

--- CommandExploreRoom.cs
+++ CommandExploreRoom.cs
@@ -3,13 +3,14 @@ class CommandExploreRoom : BaseCommand, ICommand
 {
     public CommandExploreRoom()
     {
-        description = "Show items currently in your inventory";
+        description = "See what other things are in this room";
     }
     public void Execute(Context context, string command, string[] parameters)
     {
         Space current = context.GetCurrent();
-        Item item = current.GetItem()!;
-        NPC npc = current.GetNPC()!;
+        Item? item = current.GetItem();
+        NPC? npc = current.GetNPC();
+        Monster? monster = current.Monster;
 
         //show items
         if (item == null)
@@ -28,7 +29,17 @@ class CommandExploreRoom : BaseCommand, ICommand
         }
         else
         {
-            Console.WriteLine($"NPCs in the current room: {npc.GetName()}");
+            Console.WriteLine($"NPC in the current room: {npc.GetName()}");
+        }
+
+        // Peter: Show monsters
+        if (monster == null)
+        {
+            Console.WriteLine("There are no monsters in this room.");
+        }
+        else
+        {
+            Console.WriteLine($"Monster in the current room: {monster.Name}");
         }
 
     }
