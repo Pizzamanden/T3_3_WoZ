fe268dce7a91cf9c90ef2c831e0ecf2a81be5724
Magnus Lundbech <magnus@lundbech.com>
2025-12-11 14:41:50 +0100

    Fixed naming issues

--- Commands/CommandPickUp.cs
+++ Commands/CommandPickUp.cs
@@ -45,7 +45,7 @@ class CommandPickUp : BaseCommand, ICommand
 				}
 			context.InventoryAdd(foundItem);
 			Console.WriteLine("You have picked up \"" + foundItem.GetName() + "\".");
-			context.GetNewAttack(World.D_Chemicals, World.D_Chemicals, "acid", 25, 30,"Chemical", "");
+			context.GetNewAttack(World.D_Chemicals, World.D_Chemicals, "acid", 25, 30,"chemical", "");
 			context.GetNewAttack(World.TL_Bins, World.TL_Bins, "bins", 25, 30,"recycling", Flags.Got_Bins);
 			context.GetNewAttack(World.M_Sword, World.M_Sword, "sword", 35, 40,"slice", Flags.Got_Sword);
 			//context.GetNewAttack(World.C1, World.C2, "lighter", 50, 60, "fire", Flags.Got_Lighter);
