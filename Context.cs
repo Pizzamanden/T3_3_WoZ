b346695768b472d7d27faba230be32c70623b9e5
Magnus Lundbech <magnus@lundbech.com>
2025-12-11 11:49:59 +0100

    added the ability to set random damage to new attacks

--- Context.cs
+++ Context.cs
@@ -76,11 +76,11 @@ class Context {
 	  this.current = entry;
   }
 //Troels: Method to check if the player has two specific items in their inventory to learn a new attack
-public void GetNewAttack(Item item1, Item item2, string attackName, int attackDamage, string attackType, string flag)
+public void GetNewAttack(Item item1, Item item2, string attackName, int minDamage, int maxDamage, string attackType, string flag)
     {
       if(inventory.Contains(item1) && inventory.Contains(item2) && !Player.HasAttack(attackName.ToLower()) && Flags.GetFlag(flag) == false)
       {
-        Player.AddAttack(new Attack(attackName, attackDamage, attackDamage, attackType));
+        Player.AddAttack(new Attack(attackName, minDamage, maxDamage, attackType));
         Console.WriteLine("You have learned a new attack: " + attackName + "!");
         Flags.SetFlag(flag);
       }
